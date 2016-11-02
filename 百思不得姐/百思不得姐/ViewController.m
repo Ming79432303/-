e9ce32d95391a1c2bf257881f857a1f8604f740a
Apple_Lzzy27 <apple_lzzy27@Apple-Lzzy27deMac-mini.local>
2016-11-02 08:26:52 +0800

    初始化项目

---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by Apple_Lzzy27 on 16/11/2.
+//  Copyright © 2016年 Apple_Lzzy27. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by Apple_Lzzy27 on 16/11/2.
+//  Copyright © 2016年 Apple_Lzzy27. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
